1b018cbacb249999bc71190823f7bc7b4e358207
wz2cool <wz2cool@live.cn>
2019-09-16 18:21:43 +0800

    add

--- src/@types/react-base-table.d.ts
+++ src/@types/react-base-table.d.ts
@@ -1,9 +1,6 @@
 declare module "react-base-table" {
-    export default class BaseTable extends React.PureComponent {
 
-    }
-
-    interface IColumnProp {
+    interface IColumnProps {
         /**
          * Class name for the column cell, could be a callback to return the class name
          * The callback is of the shape of `({ cellData, columns, column, columnIndex, rowData, rowIndex }) => string`
@@ -62,14 +59,83 @@ declare module "react-base-table" {
          * Whether the column is sortable, defaults to true
          */
         sortable?: boolean,
+        /**
+         * Custom column cell renderer
+         * The renderer receives props `{ cellData, columns, column, columnIndex, rowData, rowIndex, container, isScrolling }`
+         */
+        cellRenderer?: React.ReactType | React.ReactElement,
+        /**
+         * Custom column header renderer
+         * The renderer receives props `{ columns, column, columnIndex, headerIndex, container }`
+         */
+        headerRenderer?: React.ReactType | React.ReactElement,
     }
 
     /**
      * Column for BaseTable
      */
-    export class Column extends React.Component<IColumnProp, any> {
+    export class Column extends React.Component<IColumnProps, {}> {
 
     }
 
+    interface IBaseTableProps {
+        /**
+         * Prefix for table's inner className
+         */
+        classPrefix?: string,
+        /**
+         * Class name for the table
+         */
+        className?: string,
+        /**
+         * Custom style for the table
+         */
+        style?: any,
+        /**
+         * A collection of Column
+         */
+        children?: React.ReactNode,
+        /**
+         * Columns for the table
+         */
+        columns?: Column[],
+        /**
+         * The data for the table
+         */
+        data?: any,
+        /**
+         * The data be frozen to top, `rowIndex` is negative and started from `-1`
+         */
+        frozenData?: any,
+        /**
+         * The key field of each data item
+         */
+        rowKey?: "string" | "number",
+        /**
+         * The width of the table
+         */
+        width: number;
+        /**
+         * The height of the table, will be ignored if `maxHeight` is set
+         */
+        height?: number;
+        /**
+         * The max height of the table, the table's height will auto change when data changes,
+         * will turns to vertical scroll if reaches the max height
+         */
+        maxHeight?: number,
+        /**
+         * The height of each table row
+         */
+        rowHeight: number,
+        /**
+         * The height of the table header, set to 0 to hide the header, could be an array to render multi headers.
+         */
+        headerHeight: number,
+    }
+
+    export default class BaseTable extends React.PureComponent<IBaseTableProps, {}> {
+
+    }
 
 }
--- src/module/dashboard/About.tsx
+++ src/module/dashboard/About.tsx
@@ -1,10 +1,15 @@
 import BaseTable, { Column } from "react-base-table";
+import 'react-base-table'
 
 import * as React from "react";
 
 export default class About extends React.Component {
   public render() {
-    console.log(BaseTable);
-    return <div><Column key={"123"} dataKey={"1213"} align="center" width={600} frozen={"left"} /></div>;
+    return <div>
+      <BaseTable width={600} height={400} data={1} rowHeight={30} headerHeight={30} rowKey="string">
+        <Column key="col0" dataKey="col0" width={100} />
+        <Column key="col1" dataKey="col1" width={100} />
+      </BaseTable>
+    </div>;
   }
 }
